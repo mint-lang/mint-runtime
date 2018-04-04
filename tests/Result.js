07e79f4a4d55f9e00e7c9cdc8af06136f3279e6a
Szikszai Gusztáv <guszti5@hotmail.com>
2018-04-04 06:26:43 +0200

    Added tests for maybe.

--- tests/Result.js
+++ tests/Result.js
@@ -9,3 +9,7 @@ test("comparing same errors", () => {
 test("comparing with different type", () => {
   expect(compare(new Err("A"), "A")).toBe(false);
 });
+
+test("comparing same values", () => {
+  expect(compare(new Ok("A"), new Ok("A"))).toBe(true);
+});
