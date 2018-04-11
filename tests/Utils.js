c5d47b6c0c2ac48d6add1ff8bfcff241a67a1109
Gusztav Szikszai <guszti5@hotmail.com>
2018-04-11 09:20:28 +0200

    Format.

--- tests/Utils.js
+++ tests/Utils.js
@@ -1,6 +1,13 @@
 import Main from "../source/Main.js";
 
-const { compare, update, Record, navigate, insertStyles, normalizeEvent } = Main;
+const {
+  compare,
+  update,
+  Record,
+  navigate,
+  insertStyles,
+  normalizeEvent
+} = Main;
 
 console.warn = jest.fn();
 
@@ -56,39 +63,39 @@ describe("insertStyles", () => {
 
 describe("normalizeEvent", () => {
   test("retuns default values if they are not defined", () => {
-    const event = normalizeEvent({test: "X"})
+    const event = normalizeEvent({ test: "X" });
 
-    expect(() => event.clipboardData.constructor).toThrow()
-    expect(event.data).toBe("")
-    expect(event.altKey).toBe(false)
-    expect(event.charCode).toBe(-1)
-    expect(event.ctrlKey).toBe(false)
-    expect(event.key).toBe("")
-    expect(event.keyCode).toBe(-1)
-    expect(event.locale).toBe("")
-    expect(event.location).toBe(-1)
-    expect(event.metaKey).toBe(false)
-    expect(event.repeat).toBe(false)
-    expect(event.shiftKey).toBe(false)
-    expect(event.which).toBe(-1)
-    expect(event.button).toBe(-1)
-    expect(event.buttons).toBe(-1)
-    expect(event.clientX).toBe(-1)
-    expect(event.clientY).toBe(-1)
-    expect(event.pageX).toBe(-1)
-    expect(event.pageY).toBe(-1)
-    expect(event.screenY).toBe(-1)
-    expect(event.screenX).toBe(-1)
-    expect(event.detail).toBe(-1)
-    expect(event.deltaMode).toBe(-1)
-    expect(event.deltaX).toBe(-1)
-    expect(event.deltaY).toBe(-1)
-    expect(event.deltaZ).toBe(-1)
-    expect(event.animationName).toBe("")
-    expect(event.pseudoElement).toBe("")
-    expect(event.elapsedTime).toBe(-1)
-    expect(event.propertyName).toBe("")
-    expect(event.blah).toBe(undefined)
-    expect(event.test).toBe("X")
-  })
-})
+    expect(() => event.clipboardData.constructor).toThrow();
+    expect(event.data).toBe("");
+    expect(event.altKey).toBe(false);
+    expect(event.charCode).toBe(-1);
+    expect(event.ctrlKey).toBe(false);
+    expect(event.key).toBe("");
+    expect(event.keyCode).toBe(-1);
+    expect(event.locale).toBe("");
+    expect(event.location).toBe(-1);
+    expect(event.metaKey).toBe(false);
+    expect(event.repeat).toBe(false);
+    expect(event.shiftKey).toBe(false);
+    expect(event.which).toBe(-1);
+    expect(event.button).toBe(-1);
+    expect(event.buttons).toBe(-1);
+    expect(event.clientX).toBe(-1);
+    expect(event.clientY).toBe(-1);
+    expect(event.pageX).toBe(-1);
+    expect(event.pageY).toBe(-1);
+    expect(event.screenY).toBe(-1);
+    expect(event.screenX).toBe(-1);
+    expect(event.detail).toBe(-1);
+    expect(event.deltaMode).toBe(-1);
+    expect(event.deltaX).toBe(-1);
+    expect(event.deltaY).toBe(-1);
+    expect(event.deltaZ).toBe(-1);
+    expect(event.animationName).toBe("");
+    expect(event.pseudoElement).toBe("");
+    expect(event.elapsedTime).toBe(-1);
+    expect(event.propertyName).toBe("");
+    expect(event.blah).toBe(undefined);
+    expect(event.test).toBe("X");
+  });
+});
