4e93d404e489c8a19a66c3bddf03f058a0bd2bdb
Sijawusz Pur Rahnama <sija@sija.pl>
2021-07-04 02:10:25 +0200

    Encode time values in ISO 8601 format (including timezone)

--- source/Encoder.js
+++ source/Encoder.js
@@ -1,7 +1,24 @@
 import Record from "./Record";
 
 const identity = (value) => value;
-const time = (value) => +value;
+
+const time = (value) => {
+  const tzo = -value.getTimezoneOffset();
+  const dif = tzo >= 0 ? '+' : '-';
+  const pad = (num) => {
+    const norm = Math.floor(Math.abs(num));
+    return (norm < 10 ? '0' : '') + norm;
+  };
+
+  return value.getFullYear() +
+    '-' + pad(value.getMonth() + 1) +
+    '-' + pad(value.getDate()) +
+    'T' + pad(value.getHours()) +
+    ':' + pad(value.getMinutes()) +
+    ':' + pad(value.getSeconds()) +
+    dif + pad(tzo / 60) +
+    ':' + pad(tzo % 60);
+};
 
 const array = (encoder) => (value) => {
   return value.map((item) => {
